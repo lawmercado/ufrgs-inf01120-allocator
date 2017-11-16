cc25a4b95a6a211e86d80949fa0a049249634039
Nicholas de Aquino Lau <nicholas.a.lau@hotmail.com>
2017-11-16 14:16:46 -0200

    Removendo comentário de teste
    Removendo comentário de teste

--- src/allocator/algorithm/AllocationAlgorithm.java
+++ src/allocator/algorithm/AllocationAlgorithm.java
@@ -6,8 +6,6 @@ import allocator.data.domain.Allocable;
 
 public interface AllocationAlgorithm {
 	
-	// Nicho tem um teclado bugado
-	
 	public Allocable run(Allocable what, List<Allocable> where);
 	
 }
